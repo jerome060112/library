e74575d03c0d2339e3ccebe13e3c3b75609cc861
Jerome <jerome.delacruz@cloudpay.net>
2022-05-26 09:58:12 +0800

    Initial commit

---
+++ app/Http/Controllers/AuthorsController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Author;
+use Illuminate\Http\Request;
+
+class AuthorsController extends Controller
+{
+    public function index()
+    {
+        return Author::all();
+    }
+
+    public function create()
+    {
+        return view('authors.create');
+    }
+
+    public function store()
+    {
+        Author::create($this->validateRequest());
+    }
+
+    protected function validateRequest()
+    {
+        return request()->validate([
+            'name' => 'required',
+            'dob' => 'required',
+        ]);
+    }
+}
